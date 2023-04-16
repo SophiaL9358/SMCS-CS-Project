020aefb0ac70300202bfcc5fa6b9808f8d1e0127
soapity <mrows9356@gmail.com>
2023-04-15 21:57:44 -0400

    gh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  outDir: './docs',
   plugins: [svelte()],
+  build: {
+    outDir: './docs',
+  }
 })
