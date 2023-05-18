851bc69d15417ed292bc825864d2e11919c180dd
soapity <mrows9356@gmail.com>
2023-05-18 14:03:44 -0400

    SACHET IS AMAZING THANK YOU

--- src/lib/SignIn.svelte
+++ src/lib/SignIn.svelte
@@ -4,15 +4,25 @@
 
     let idInput;
     let passwordInput;
+    let warning = "";
     const DISTRICT_URL = "https://md-mcps-psv.edupoint.com/PXP2_Login.aspx";
     async function handleClick(){
         try {
             let response = await StudentVue.login(DISTRICT_URL, { username: idInput.value, password: passwordInput.value });
             console.log("user exists!");
+            
+
+            user.update(state => ({...state, 
+                email: idInput.value + "@mcpsmd.net",
+                name: response.studentInfo().then.name,
+                loggedIn: true,
+                grade: 2025 // TODO: Assigns grade based on database
+            }));
             idInput.value = "";
             passwordInput.value = "";
         } catch {
             console.log("user doesn't exist!");
+            warning = "Username or Password is incorrect!";
         }
     }
 </script>
@@ -21,3 +31,4 @@ ID: <input type = "text" bind:this = {idInput}> <br>
 Password: <input type = "password" bind:this = {passwordInput}>
 <br> <br>
 <button on:click = {handleClick}>Submit</button>
+<p>{warning}</p>
