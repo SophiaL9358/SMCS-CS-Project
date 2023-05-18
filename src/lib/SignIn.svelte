d79bb457a7de6736fcfc0e8d42af2dafac8862c9
soapity <mrows9356@gmail.com>
2023-05-18 15:08:00 -0400

    shelved confimration page

--- src/lib/SignIn.svelte
+++ src/lib/SignIn.svelte
@@ -1,25 +1,35 @@
 <script>
     import StudentVue from 'studentvue';
-    import { user } from './constants';
+    import { user, sidebar_width_em, updateSize } from './constants';
 
     let idInput;
     let passwordInput;
     let warning = "";
     const DISTRICT_URL = "https://md-mcps-psv.edupoint.com/PXP2_Login.aspx";
     async function handleClick(){
+        warning = "Processing...";
         try {
             let response = await StudentVue.login(DISTRICT_URL, { username: idInput.value, password: passwordInput.value });
             console.log("user exists!");
             
-
+            console.log(response.studentInfo().then());
             user.update(state => ({...state, 
                 email: idInput.value + "@mcpsmd.net",
-                name: response.studentInfo().then.name,
+                name: idInput.value,
                 loggedIn: true,
+                confirmed: true, // TODO: Gets rid of confirmation page
                 grade: 2025 // TODO: Assigns grade based on database
             }));
             idInput.value = "";
             passwordInput.value = "";
+            response = null;
+
+            // TODO: GET RID OF THIS IF PUTTING BACK CONFIRMATION PAGE:
+            sidebar_width_em.set({
+                display: "block",
+                width: $sidebar_width_em.width
+            });
+            updateSize();
         } catch {
             console.log("user doesn't exist!");
             warning = "Username or Password is incorrect!";
